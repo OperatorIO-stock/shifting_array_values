994179bb80e00fbe3e9fb0c4d9f7767128d415ae
OperatorIO-stock <vova.chigorin@yandex.com>
2025-01-17 17:19:03 +0300

    commit GO

--- Program.cs
+++ Program.cs
@@ -4,19 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] numbers = new int[5];
-        int lastIndex = numbers.Length - 1;
-        int number = 0;
-
-        int tempNumbers;
+        int[] numbers = new int[4];
+        int length = numbers.Length;
+        int firstElement = numbers[0];
 
         string userResponse;
 
-        for (int i = 0; i < numbers.Length; i++)
+        for (int i = 0; i < length; i++)
         {
-            number++;
-            numbers[i] = number;
-
+            numbers[i] = i + 1;
             Console.Write($"{numbers[i]} ");
         }
         
@@ -26,17 +22,20 @@ class Program
 
         if (int.TryParse(userResponse, out int countLeftShift))
         {
+            countLeftShift %= length;
+            
             for (int i = 0; i < countLeftShift; i++)
-                for (int j = 0; j < lastIndex; j++)
+            {
+                for (int j = 0; j < length - 1; j++)
                 {
-                    tempNumbers = numbers[j];
                     numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = tempNumbers;
                 }
-        }
 
+                numbers[length - 1] = firstElement;
+            }
+        }
         else
-            { Console.WriteLine("That's not number!"); }
+            Console.WriteLine("That's not number!");
 
         Console.Write(string.Join(" ", numbers));
     }
